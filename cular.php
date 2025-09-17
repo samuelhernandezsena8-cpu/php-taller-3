5459a68ad74182d3d6e1116e8665faab05143954
samuelhernandezsena8-cpu <samuelhernandezsena8@gmail.com>
2025-09-16 19:16:15 -0500

    Update cular.php

--- cular.php
+++ cular.php
@@ -1,1 +1,19 @@
+<?php
+$N = $_GET ['N'];
+$C = $_GET ['C'];
+$U = $_GET ['U']; 
+$operacion;
+$operacion = $U * $U * $U;
 
+
+echo "Su nombre es:".$N;
+echo "<br>";
+echo "Su curso es:".$C;
+echo "<br>";
+if($U >=100 ) {
+    echo "FELICIDADES ERES AFORTUNADO".$operacion;
+}
+else {
+    echo "su numero es:".$U;
+}
+?>
